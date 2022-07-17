493f23895bc092832318b6b220a42a17f3db5e59
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-07-17 14:42:57 +0100

    Added two pictures and updated server.ts and package.json

--- src/server.ts
+++ src/server.ts
@@ -3,25 +3,31 @@ import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
-
+  
   // Init the Express application
   const app = express();
-
+  
   // Set the network port
   const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
+  
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
   app.get("/filteredimage", async (req, res) => {
     const { image_url }: { image_url: string } = req.query;
     
     if (!image_url) {
-      res.status(400).send({ message: "image_url is required or malformed" });
+      res.status(400).send({ message: "image_url cannot be empty" });
       return;
     }
 
@@ -63,11 +69,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       });
   });
   
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
   
 
   // Start the Server
